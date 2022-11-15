b8256b257f0028e2643d4c745c043895d88aead6
F.Kanayama <fkanayam@redhat.com>
2022-11-15 18:36:52 +0900

    Let code scanner failed

--- src/fuga.py
+++ src/fuga.py
@@ -1,2 +1,20 @@
 def fuga(x: int, y: int) -> int:
     return x - y
+
+
+PASSWORD='ju8Kwkq23jxuDje'
+
+# Meaning-less strings over 128 chars in 1 line: xxxxxxxxxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxxx
+def suspiciousPythonGrammerFunc(x):
+    print(PASSWORD)
+    if (True):
+        if (True):
+            if (True):
+                if (True):
+                    if (True):
+                        if (True):
+                            if (True):
+                                return 0
+    return 1
+
+    
