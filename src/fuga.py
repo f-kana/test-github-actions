377faee972c7ed6f301a72c34183d7987e3cb16e
F.Kanayama <fkanayam@redhat.com>
2022-11-15 19:03:57 +0900

    update a little

--- src/fuga.py
+++ src/fuga.py
@@ -5,7 +5,7 @@ def fuga(x: int, y: int) -> int:
 PASSWORD='ju8Kwkq23jxuDje'
 
 # Meaning-less strings over 128 chars in 1 line: xxxxxxxxxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxxx
-def suspiciousPythonGrammerFunc(x):
+def suspiciousPythonGrammerFunc_WithMeaningLessCharsLike_xxxxxxx_yyyyyyyy_zzzzzzzzz_happyHoppyHuppy(x):
     print(PASSWORD)
     if (True):
         if (True):
